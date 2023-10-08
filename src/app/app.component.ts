9f3a2ed054b5378cee0aaf81d5b1c9cd2b8a3be9
Lasha <lashakakashvili114@gmail.com>
2023-10-08 22:02:49 +0400

    Testing Calendar Capabilities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,159 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { CalendarEvent } from 'angular-calendar';
+import { NzModalService } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'calendar';
+
+  viewDate: Date = new Date();
+
+  data = [
+    {
+      id: 1,
+      companyName: 'It Step Academy',
+      groupName: 'JP-WS-19',
+      grade: 'Middle',
+      status: 0,
+      startDate: '2023-10-07T10:04:33.799',
+      endDate: '2024-04-07T10:04:33.799',
+      currentSession: 0,
+      sessionsAmount: 52,
+      currentSyllabusTopicId: null,
+      sessions: [
+        {
+          id: 1,
+          day: 2,
+          time: '16:00-18:00',
+          auditorium: '32',
+          isOnline: false,
+          isAlternate: false,
+        },
+        {
+          id: 2,
+          day: 4,
+          time: '16:00-18:00',
+          auditorium: '32',
+          isOnline: false,
+          isAlternate: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      companyName: 'Tlancer',
+      groupName: 'WS-3116',
+      grade: 'Junior',
+      status: 0,
+      startDate: '2023-10-08T17:59:21.548',
+      endDate: '2024-04-08T17:59:21.548',
+      currentSession: 0,
+      sessionsAmount: 52,
+      currentSyllabusTopicId: null,
+      sessions: [
+        {
+          id: 53,
+          day: 1,
+          time: '20:00-22:00',
+          auditorium: '51',
+          isOnline: true,
+          isAlternate: true,
+        },
+        {
+          id: 54,
+          day: 3,
+          time: '20:00-22:00',
+          auditorium: '51',
+          isOnline: true,
+          isAlternate: true,
+        },
+      ],
+    },
+  ];
+  events: CalendarEvent[] = [];
+  private lastDayProcessed: number | null = null;
+  hoveredEvent: CalendarEvent | null = null;
+  modalRef: any = null; // keep a reference to the modal
+  daySchedulingCount: { [key: number]: number } = {}; // New mapping to keep track
+
+  constructor(private _modalService: NzModalService) {}
+  @ViewChild('nzTemplate', { static: false }) nzTemplate!: TemplateRef<any>;
+  ngOnInit() {
+    this.generateEvents();
+  }
+  showTooltip(event: any) {
+    this.hoveredEvent = event.event;
+    this.modalRef = this._modalService.create({
+      nzTitle: 'Event Details',
+      nzContent: this.nzTemplate,
+      nzFooter: null,
+      nzClosable: false,
+      nzMask: false,
+      nzMaskClosable: false,
+    });
+  }
+
+  hideTooltip() {
+    this.hoveredEvent = null;
+  }
+
+  generateEvents() {
+    this.data.forEach((group) => {
+      group.sessions
+        .sort((a, b) => a.day - b.day)
+        .forEach((session) => {
+          const startHour = session.time.split('-')[0];
+          const endHour = session.time.split('-')[1];
+
+          // If the day hasn't been scheduled yet, initialize it
+          if (!this.daySchedulingCount[session.day]) {
+            this.daySchedulingCount[session.day] = 0;
+          }
+
+          const dateForSession = this.getUpcomingDateForDay(
+            session.day,
+            this.daySchedulingCount[session.day]
+          );
+
+          // Increase the scheduling count for that day
+          this.daySchedulingCount[session.day]++;
+
+          const start = new Date(`${dateForSession}T${startHour}:00`);
+          const end = new Date(`${dateForSession}T${endHour}:00`);
+
+          this.events.push({
+            start,
+            end,
+            title: `${group.groupName} | ${session.auditorium} | ${startHour}-${endHour}`,
+            meta: {
+              detail: `${group.companyName} | ${group.groupName} (${group.grade}) | Auditorium: ${session.auditorium} | Time: ${startHour} to ${endHour}`,
+            },
+          });
+        });
+    });
+  }
+
+  private getUpcomingDateForDay(day: number, weeksToSkip: number = 0): string {
+    const now = new Date();
+    let daysToAdd = ((day - now.getDay() + 7) % 7) + 7 * weeksToSkip; // Using weeksToSkip to skip the necessary weeks
+
+    now.setDate(now.getDate() + daysToAdd);
+    return now.toISOString().split('T')[0];
+  }
+
+  prevWeek(): void {
+    const date = new Date(this.viewDate);
+    date.setDate(date.getDate() - 7);
+    this.viewDate = date;
+  }
+
+  nextWeek(): void {
+    const date = new Date(this.viewDate);
+    date.setDate(date.getDate() + 7);
+    this.viewDate = date;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {NzModalModule} from 'ng-zorro-antd/modal';
+registerLocaleData(en); // if you haven't imported this already
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NzToolTipModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+    FormsModule,
+    NzModalModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
